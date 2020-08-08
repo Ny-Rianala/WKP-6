ff49571ba371c5d789d9aa49ee791ee1df89448b
kati <fanomezantsoakatiuscia048@gmail.com>
2020-08-08 17:08:10 +0300

    added new files

---
+++ script.js
@@ -0,0 +1,81 @@
+const foods = [
+	{
+		id: 'ravitoto',
+		price: 5000,
+		title: 'Ravitoto',
+		spicy: true,
+		vegetarian: false,
+	},
+	{
+		id: 'pasta',
+		price: 4000,
+		title: 'Pasta',
+		spicy: true,
+		vegetarian: true,
+	},
+	{
+		id: 'burger',
+		price: 5000,
+		title: 'Burger',
+		spicy: false,
+		vegetarian: false,
+	},
+	{
+		id: 'rice',
+		price: 2000,
+		title: 'Rice and Leaves',
+		spicy: false,
+		vegetarian: true,
+	},
+	{
+		id: 'mofogasy',
+		price: 500,
+		title: 'Mofogasy',
+		spicy: false,
+		vegetarian: false,
+	},
+];
+
+const foodDelivery = document.querySelector('.container');
+const listOfFood = document.querySelector('.list-of-food');
+const spicyFood = document.querySelector('.spicy-food');
+const vegetarianFood = document.querySelector('.vegetarian-food');
+
+
+//Generate list for all foods
+const foodCard = (e) => {
+    //add html list
+        const listHTML = foods.map (food => `
+          <ul class= "list-of-food">
+            <li>${food.title} ${food.price}</li>
+          </ul>
+        <button>Add</button>`
+        );
+            listOfFood.innerHTML = listHTML;
+    };
+    foodCard();
+
+
+//check the food if they are spicy or vegetarian
+const tasteOfFood = id => {
+    // const taste = e.currentTarget.item.value;
+    if (spicyFood.checked === true) {
+       const spicyTaste = foods.filter(spicy => spicy === true);
+          spicyTaste.map(spicy => ` 
+              <ul>
+                <li>${spicy.id}</li>
+              </ul> 
+       `);
+    }else if(vegetarianFood.checked === true) {
+        const vegetarianTaste = foods.filter(vegetarian => vegetarian === true);
+        vegetarianTaste.map(vegetarian => `
+        <ul>
+           <li>${spicy.id}</li>
+        </ul>
+        `);
+    }
+};
+
+//Listen for the checkbox
+spicyFood.addEventListener('change', tasteOfFood);
+vegetarianFood.addEventListener('change', tasteOfFood);
